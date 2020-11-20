8f46527a65fdd93467c0a01e833c9ed1b3c68517
Giacomo <giacomo.1936@gmail.com>
2020-11-20 16:39:06 +0100

    creata lista mail

---
+++ main.js
@@ -0,0 +1,23 @@
+var app = new Vue({
+    el: '#root',
+    data: {
+        nome_paragrafo:,
+        //creo una lista email dove pushare tutte le mail prese
+        lista_email: [
+
+        ]
+    },
+    //una volta caricata ciclo per 10 volte una mail random
+    mounted() {
+        //imposto il ciclo fino a 9
+        for (var i = 0; i < 10; i++) {
+            const self = this;
+            //prendo l'API della mail random generata e replico fin quando non arrivo a 10 mail
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                //creo una funzione dove pusherò ogni volta la mail pescata nella lista
+                .then(function(contact) {
+                    self.lista_email.push(contact.data.response)
+                });
+            }
+        }
+    });
